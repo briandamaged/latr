3516bec0690e7590a07c08d186b919695f0b6fcf
Brian Lauber <constructile.truth@gmail.com>
2013-01-17 01:01:58 -0500

    Added some implementations for some basic lazy iterators.

---
+++ rupy/__init__.py
@@ -0,0 +1,66 @@
+
+def empty():
+  """
+  Returns an empty iterator
+  """
+  return iter(())
+
+def identity(iterable):
+  for item in iterable:
+    yield item
+
+
+class LMap(object):
+  def __init__(self, map_function):
+    self.map_function = map_function
+
+  def __call__(self, iterable):
+    for item in iterable:
+      yield self.map_function(item)
+
+
+
+
+class LFilter(object):
+  def __init__(self, filter_function):
+    self.filter_function = filter_function
+
+  def __call__(self, iterable):
+    for item in iterable:
+      if self.filter_function(item):
+        yield item
+
+
+class Cycle(object):
+  def __init__(self):
+    self.__memory = []
+
+  def __call__(self, iterable):
+    for item in iterable:
+      self.__memory.append(item)
+      yield item
+    while True:
+      for item in self.__memory:
+        yield item
+
+class Rupy(object):
+  def __init__(self, iterable):
+    self.iterable = iter(iterable)
+
+  def next(self):
+    return self.iterable.next()
+
+  def __iter__(self):
+    return self
+
+  def __lshift__(self, consumer):
+    return Rupy(consumer(self.iterable))
+
+
+
+
+def lmap(map_function, iterable):
+  return LMap(map_function)(iterable)
+
+def lfilter(filter_function, iterable):
+  return LFilter(filter_function)(iterable)
