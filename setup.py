0db45ff8080a16a6b38cf41503ae9ecd161d5b76
Brian Lauber <constructile.truth@gmail.com>
2013-01-16 23:19:05 -0500

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'rupy',
+    version = '0.0.1',
+    description = 'Ruby-style iterators... for Python!',
+    author = 'Brian Lauber',
+    author_email = 'constructible.truth@gmail.com',
+    packages = ['rupy'],
+    test_suite = 'tests',
+)
+
