0554f08335ff0aff62be183566de7e1f6ed0d35b
Brian Lauber <constructile.truth@gmail.com>
2013-01-18 22:56:25 -0500

    Incremented the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'latr',
-    version = '0.0.3',
+    version = '0.0.4',
     description = 'Lazy Iterator Chains for Python',
     author = 'Brian Lauber',
     author_email = 'constructible.truth@gmail.com',
